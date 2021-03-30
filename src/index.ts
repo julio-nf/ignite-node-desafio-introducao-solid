52bf1a85c36ff8d3ab7e01f9fe484d72219e5f9f
Julio Novais <julio.novais@outlook.com>
2021-03-30 16:15:23 -0300

    feat: added swagger documentation

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 import express from 'express';
+import path from 'path';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 
 import { usersRoutes } from './routes/users.routes';
+
+const swaggerDocumentPath = path.resolve(__dirname, './swagger.yaml');
+
+const swaggerDocument = YAML.load(swaggerDocumentPath);
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/users', usersRoutes);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export { app };
